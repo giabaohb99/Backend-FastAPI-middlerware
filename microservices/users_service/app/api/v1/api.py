7a5dee59926915c47787b785e4d32781c64cdaea
giabaohb99 <hb.giabao99@gmail.com>
2025-03-11 20:16:55 +0700

    update

--- microservices/users_service/app/api/v1/api.py
+++ microservices/users_service/app/api/v1/api.py
@@ -25,14 +25,14 @@ def register(*, db: Session = Depends(get_db), user_in: UserCreate) -> Any:
     user = get_user_by_email(db, email=user_in.email)
     if user:
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
             detail="Email already registered"
         )
     
     user = get_user_by_username(db, username=user_in.username)
     if user:
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
             detail="Username already registered"
         )
     
@@ -56,7 +56,7 @@ def login(
         )
     elif user.status != UserStatus.ACTIVE:
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
             detail=f"User account is {UserStatus.get_description(user.status).lower()}"
         )
     
